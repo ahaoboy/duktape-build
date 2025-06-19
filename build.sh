0f841c32c8eae3bbb1788e14beb75942f339b772
ahaoboy <504595380@qq.com>
2025-06-19 09:48:18 +0800

    init

---
+++ build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+    echo "not found target"
+    exit 1
+fi
+
+TARGET=$1
+
+curl -L -o duktape.tar.xz https://github.com/svaarala/duktape/releases/download/v2.7.0/duktape-2.7.0.tar.xz
+tar xvfJ duktape.tar.xz
+cd duktape-2.7.0
+
+make -f Makefile.cmdline
+
+mkdir ../dist
+
+cp ./duk ../dist
+
+cd ..
+
+ls -lh dist
+
+tar -czf ./duktape-${TARGET}.tar.gz -C dist .
+ls -l ./duktape-${TARGET}.tar.gz
