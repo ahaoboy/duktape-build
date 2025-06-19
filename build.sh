b0923d1a525c027f39cdd7b4f56429bc92cf1588
ahaoboy <504595380@qq.com>
2025-06-19 12:19:43 +0800

    init

--- build.sh
+++ build.sh
@@ -7,11 +7,16 @@ fi
 
 TARGET=$1
 
-curl -L -o duktape.tar.xz https://github.com/svaarala/duktape/releases/download/v2.7.0/duktape-2.7.0.tar.xz
-tar xvfJ duktape.tar.xz
-cd duktape-2.7.0
-
-make -f Makefile.cmdline
+if [[ "$TARGET" == *windows* ]]; then
+  curl -L -o duktape.tar.xz https://github.com/svaarala/duktape/releases/download/v2.7.0/duktape-2.7.0.tar.xz
+  tar xvfJ duktape.tar.xz
+  cd duktape-2.7.0
+  make -f Makefile.cmdline
+else
+  git clone https://github.com/svaarala/duktape duktape
+  cd duktape
+  make duk
+fi
 
 mkdir ../dist
 
